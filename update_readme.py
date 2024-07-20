c07ed906de322aa06f45331ae59b74a37ecbe324
Mikhail Kopochinskiy <rokcrokcet@gmail.com>
2024-07-20 20:07:13 +0500

    processing for the readme.md file, different inflections of the word 'day' are now available

--- update_readme.py
+++ update_readme.py
@@ -10,6 +10,13 @@ CURRENT_DATE = datetime.datetime.now()
 # My experience in days
 EXPERIENCE_DAYS = (CURRENT_DATE - START_DATE).days
 
+# Correct form of the word "day" (Russian version only)
+DAY_WORD_FORM_RU = {
+    (0, 5, 6, 7, 8, 9): 'дней',
+    (2, 3, 4): 'дня',
+    (1,): 'день'
+}
+
 # Directory with template files for update
 TEMPLATES_DIR = 'templates'
 
@@ -25,6 +32,16 @@ TEMPLATE_FILES = [
     'README_CN_template.md', 'README_ES_template.md', 'README_UA_template.md'
 ]
 
+
+def get_day_word_form_ru(days_value):
+    """Gets the required form of a word from the dictionary."""
+    last_digit = int(str(days_value)[-1])
+    for key, value in DAY_WORD_FORM_RU.items():
+        if last_digit in key:
+            return value
+    return ''
+
+
 for template_file, output_file in zip(TEMPLATE_FILES, OUTPUT_FILES):
     template_path = os.path.join(TEMPLATES_DIR, template_file)
     with open(template_path, 'r', encoding='utf-8') as file:
@@ -33,6 +50,14 @@ for template_file, output_file in zip(TEMPLATE_FILES, OUTPUT_FILES):
     updated_content = content.replace(
         '{EXPERIENCE_DAYS}', str(EXPERIENCE_DAYS)
     )
+
+    if '{DAY_WORD_FORM_RU}' in content:
+        last_digit_in_days = int(str(EXPERIENCE_DAYS)[-1])
+        if last_digit_in_days in DAY_WORD_FORM_RU.keys():
+            day_word_form = get_day_word_form_ru(last_digit_in_days)
+            updated_content = updated_content.replace(
+                '{DAY_WORD_FORM_RU}', day_word_form
+            )
 
     with open(output_file, 'w', encoding='utf-8') as file:
         file.write(updated_content)
